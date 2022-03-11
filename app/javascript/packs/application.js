afc950e8b9373be0c74fcf86cf5c703eecf57de0
Fumiya-Matsumoto <ekiden238@gmail.com>
2022-03-11 17:55:00 +0900

    Enable to use jQuery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery"
-import "jquery.matchHeight.js"
+
 
 Rails.start()
 Turbolinks.start()
