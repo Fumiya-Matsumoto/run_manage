60eb86b0d5a900d3afae66bd4872a4e260039a71
Fumiya-Matsumoto <ekiden238@gmail.com>
2022-03-16 22:49:49 +0900

    improve post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,11 +19,11 @@ class PostsController < ApplicationController
 
             # 入力値をint型に変換
             params[:post][:post_records_attributes][num][:time_hour] = 
-            params[:post][:post_records_attributes][num][:time_hour].to_i
+            params[:post][:post_records_attributes][num][:time_hour].to_f
             params[:post][:post_records_attributes][num][:time_minute] = 
-            params[:post][:post_records_attributes][num][:time_minute].to_i
+            params[:post][:post_records_attributes][num][:time_minute].to_f
             params[:post][:post_records_attributes][num][:time_second] = 
-            params[:post][:post_records_attributes][num][:time_second].to_i
+            params[:post][:post_records_attributes][num][:time_second].to_f
 
             # レスト以外ではペースを計算する
             if not params[:post][:kind_of_practice] == "レスト"
@@ -31,13 +31,16 @@ class PostsController < ApplicationController
                     3600 * params[:post][:post_records_attributes][num][:time_hour] + 
                     60 * params[:post][:post_records_attributes][num][:time_minute] + 
                     params[:post][:post_records_attributes][num][:time_second]
-                pace = total_seconds / params[:post][:post_records_attributes][num][:distance_km].to_i
+                pace = total_seconds / params[:post][:post_records_attributes][num][:distance_km].to_f
                 pace_minute = pace.div(60)
                 pace_second = pace % 60
-                pace_undersecond = pace - pace.to_i
+                pace_undersecond = pace - pace.to_f
                 params[:post][:post_records_attributes][num][:pace_minute] = pace_minute
                 params[:post][:post_records_attributes][num][:pace_second] = pace_second
                 params[:post][:post_records_attributes][num][:pace_undersecond] = pace_undersecond
+                if params[:post][:kind_of_practice] == "ペース走" or params[:post][:kind_of_practice] == "ビルドアップ" or params[:post][:kind_of_practice] == "インターバル" or params[:post][:kind_of_practice] == "レペティション" or params[:post][:kind_of_practice] == "レース"
+                    params[:post][:post_records_attributes][num][:distance_m] = params[:post][:post_records_attributes][num][:distance_km] * 1000
+                end
             end
         end
 
@@ -54,6 +57,7 @@ class PostsController < ApplicationController
         @post.practice_day_day = @post.practice_day.day
         days = ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"]
         @post.practice_day_of_week = days[@post.practice_day.wday]
+        
         
         if @post.save
             flash[:notice] = "投稿を作成しました"
