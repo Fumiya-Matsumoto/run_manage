7944a3c313912dbf92a6337f8e3399528f991f00
Fumiya-Matsumoto <ekiden238@gmail.com>
2022-03-20 00:30:07 +0900

    Enable to destroy post

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get "users/:id/:year/:month" => "users#show"
   get "posts/new" => "posts#new"
   post "posts/create" => "posts#create"
+  post "posts/:id/destroy_show" => "posts#destroy_show"
+  post "posts/:id/destroy_main" => "posts#destroy_main"
   root 'home#top'
 end
