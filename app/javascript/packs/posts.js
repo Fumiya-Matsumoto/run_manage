ff425e870f65b76af957f5690342d7013375f272
radkmb <rad.kmb@gmail.com>
2022-03-20 19:50:37 +0900

    Fix JS

--- app/javascript/packs/posts.js
+++ app/javascript/packs/posts.js
@@ -2,25 +2,27 @@ document.addEventListener("turbolinks:load", function () {
     $(function(){
         var switchBtn = $('input[name="post[kind_of_practice]"]');
         var switchItems = $('.switchItems');
-        var switchItems1 = $('.result-header > .time')
-        var switchItems2 = $('.high')
-        switchItems2.hide()
+        var switchItems1 = $('.result-header > .time');
+        var switchItems2 = $('.high');
+        switchItems2.hide();
         switchBtn.on('change', function(){
             var inputValue = $(this).val();
-            if (inputValue === "レスト"){
+            if (inputValue === "ジョグ") {
+                $('.high').hide();
+            } else if (inputValue === "レスト") {
                 switchItems.hide();
-            }else if (inputValue === "インターバル" || inputValue === "レペティション"){
+            } else if (inputValue === "インターバル" || inputValue === "レペティション") {
                 switchItems.show();
                 switchItems1.hide();
                 switchItems2.show();
                 $('.distance-text').text('[m]');
-            }else{
+            } else {
                 switchItems.show();
                 switchItems1.show();
                 switchItems2.hide();
-                if (inputValue === "ジョグ" || inputValue === "距離走"){
+                if (inputValue === "ジョグ" || inputValue === "距離走") {
                     $('.distance-text').text('[km]');
-                }else{
+                } else {
                     $('.distance-text').text('[m]');
                 }
             }
