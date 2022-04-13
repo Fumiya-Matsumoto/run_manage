2968d62780e226affd00f5c48c02e0a14e478abe
Fumiya-Matsumoto <ekiden238@gmail.com>
2022-04-13 19:10:00 +0900

    Introduce chart.js and create mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,8 @@ class UsersController < ApplicationController
       @posts = Post.where(user_id: @user.id).order("practice_day DESC")
       @days = ["日", "月", "火", "水", "木", "金", "土"]
     end
+
+    def mypage
+      @distance_labels = ["1月", "2月", "3月", "4月", "5月", "6月", '7月', "8月", "9月", "10月", "11月", "12月"].to_json.html_safe
+    end
   end
